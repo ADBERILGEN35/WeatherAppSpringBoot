dbc2cfd4b1f2853238d7f816093361f77c1504ea
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-24 18:41:45 +0300

    Weather App || WeatherService updated.

--- src/main/java/com/adberilgen/weather/dto/WeatherDto.java
+++ src/main/java/com/adberilgen/weather/dto/WeatherDto.java
@@ -1,8 +1,14 @@
 package com.adberilgen.weather.dto;
 
+import com.adberilgen.weather.model.WeatherEntity;
+
 public record WeatherDto(
         String cityName,
         String country,
         Integer temperature
 ) {
+    public static WeatherDto convert(WeatherEntity from) {
+        return new WeatherDto(from.getCityName(), from.getCountry(), from.getTemperature());
+    }
+
 }
--- src/main/java/com/adberilgen/weather/service/WeatherService.java
+++ src/main/java/com/adberilgen/weather/service/WeatherService.java
@@ -1,20 +1,60 @@
 package com.adberilgen.weather.service;
 
 import com.adberilgen.weather.dto.WeatherDto;
+import com.adberilgen.weather.dto.WeatherResponse;
+import com.adberilgen.weather.model.WeatherEntity;
 import com.adberilgen.weather.repository.WeatherRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Service
 public class WeatherService {
 
     private final WeatherRepository weatherRepository;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String API_URL = "http://api.weatherstack.com/current?access_key=e36c2fd8b5a1a0919445d0a3f00576aa&query=";
 
-    public WeatherService(WeatherRepository weatherRepository) {
+    public WeatherService(WeatherRepository weatherRepository, RestTemplate restTemplate, RestTemplate restTemplate1) {
         this.weatherRepository = weatherRepository;
+        this.restTemplate = restTemplate;
     }
 
     public WeatherDto getWeatherByCityName(String city) {
+        Optional<WeatherEntity> weatherEntityOptional = weatherRepository.findFirstByRequestedCityNameOrderByUpdatedTimeDesc(city);
+        if (weatherEntityOptional.isPresent()) {
+            return WeatherDto.convert(getWeatherFromWeatherStack(city));
+        }
+        return WeatherDto.convert(weatherEntityOptional.get());
+    }
 
-        return null;
+    public WeatherEntity getWeatherFromWeatherStack(String city) {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(API_URL + city, String.class);
+
+        try {
+            WeatherResponse weatherResponse = objectMapper.readValue(responseEntity.getBody(), WeatherResponse.class);
+            return saveWeatherEntity(city, weatherResponse());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private WeatherEntity saveWeatherEntity(String city, WeatherResponse weatherResponse) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        WeatherEntity weatherEntity = new WeatherEntity(city,
+                weatherResponse.location().name(),
+                weatherResponse.location().country(),
+                weatherResponse.current().tempature(),
+                LocalDateTime.now(),
+                LocalDateTime.parse(weatherResponse.location().localTime(), dateTimeFormatter)
+        );
+        return weatherRepository.save(weatherEntity);
     }
 }
