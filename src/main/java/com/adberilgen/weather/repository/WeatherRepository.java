4c34326734c1f4fc601cabaab3dbdfe32c0680ab
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-24 18:14:05 +0300

    Weather App || Repository query added.

--- src/main/java/com/adberilgen/weather/repository/WeatherRepository.java
+++ src/main/java/com/adberilgen/weather/repository/WeatherRepository.java
@@ -4,6 +4,11 @@ import com.adberilgen.weather.model.WeatherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WeatherRepository extends JpaRepository<WeatherEntity, String> {
+
+    //Son kaydın ne zaman atıldığını bulmak için kullanılan sorgu
+    Optional<WeatherEntity> findFirstByRequestedCityNameOrderByUpdatedTimeDesc(String city);
 }
