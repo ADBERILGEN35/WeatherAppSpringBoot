9cf78187012e83d0938a2bfd8b8cd7ad6fe15e2c
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-29 12:55:12 +0300

    Weather App || Constants updated.

--- src/main/java/com/adberilgen/weather/constants/Constants.java
+++ src/main/java/com/adberilgen/weather/constants/Constants.java
@@ -1,20 +1,22 @@
 package com.adberilgen.weather.constants;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Constants {
     public static String API_URL;
     public static String API_KEY;
     public static String ACCESS_KEY_PARAM = "?access_key=";
-    public static String QUERY_KEY_PARAM = "?&query==";
+    public static String QUERY_KEY_PARAM = "&query==";
 
     @Value("${weather-stack.api-url}")
     public static void setApiUrl(String apiUrl) {
-        API_URL = apiUrl;
+        Constants.API_URL = apiUrl;
     }
 
     @Value("${weather-stack.api-key}")
     public static void setApiKey(String apiKey) {
-        API_KEY = apiKey;
+        Constants.API_KEY = apiKey;
     }
 }
