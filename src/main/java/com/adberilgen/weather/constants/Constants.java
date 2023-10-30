a8e3003d700cf0b3ca15b3e731e0f7acfedecc6a
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-30 20:42:52 +0300

    Weather App || Constants updated.

--- src/main/java/com/adberilgen/weather/constants/Constants.java
+++ src/main/java/com/adberilgen/weather/constants/Constants.java
@@ -5,18 +5,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Constants {
-    public static String API_URL;
+    public static String WEATHER_STACK_API_BASE_URL;
+    public static final String WEATHER_STACK_API_ACCESS_KEY_PARAM = "?access_key=";
+    public static final String WEATHER_STACK_API_QUERY_PARAM = "&query=";
+
     public static String API_KEY;
-    public static String ACCESS_KEY_PARAM = "?access_key=";
-    public static String QUERY_KEY_PARAM = "&query==";
+    public static Integer API_CALL_LIMIT;
+    public static String WEATHER_CACHE_NAME;
 
     @Value("${weather-stack.api-url}")
-    public static void setApiUrl(String apiUrl) {
-        Constants.API_URL = apiUrl;
+    public void setWeatherStackApiBaseUrl(String apiUrl) {
+        Constants.WEATHER_STACK_API_BASE_URL = apiUrl;
+    }
+
+    @Value("${weather-stack.cache-name}")
+    public void setWeatherCacheName(String cacheName) {
+        Constants.WEATHER_CACHE_NAME = cacheName;
     }
 
     @Value("${weather-stack.api-key}")
-    public static void setApiKey(String apiKey) {
-        Constants.API_KEY = apiKey;
+    public void setApiKey(String apiKey) {
+        API_KEY = apiKey;
+    }
+
+    @Value("${weather-stack.api-call-limit}")
+    public void setApiCallLimit(Integer apiCallLimit) {
+        API_CALL_LIMIT = apiCallLimit;
     }
 }
