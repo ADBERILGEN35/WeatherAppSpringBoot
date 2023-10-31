fd06f9b64f71a7ca8d7e7d4ae6f1ca92762f7463
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-31 19:25:23 +0300

    Weather App || Cache added.

--- src/main/java/com/adberilgen/weather/service/WeatherService.java
+++ src/main/java/com/adberilgen/weather/service/WeatherService.java
@@ -6,7 +6,14 @@ import com.adberilgen.weather.model.WeatherEntity;
 import com.adberilgen.weather.repository.WeatherRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,18 +24,23 @@ import java.util.Optional;
 import static com.adberilgen.weather.constants.Constants.*;
 
 @Service
+@CacheConfig(cacheNames = {"weathers"})
 public class WeatherService {
 
     private final WeatherRepository weatherRepository;
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherService.class);
+
     public WeatherService(WeatherRepository weatherRepository, RestTemplate restTemplate, RestTemplate restTemplate1) {
         this.weatherRepository = weatherRepository;
         this.restTemplate = restTemplate;
     }
 
+    @Cacheable(key = "#city")
     public WeatherDto getWeatherByCityName(String city) {
+        logger.info("Requested city : " + city);
         Optional<WeatherEntity> weatherEntityOptional = weatherRepository.findFirstByRequestCityNameOrderByUpdatedTimeDesc(city);
         return weatherEntityOptional.map(weather -> {
             if (weather.getUpdatedTime().isBefore(LocalDateTime.now().minusMinutes(30))) {
@@ -47,6 +59,13 @@ public class WeatherService {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "100000")
+    public void clearCache() {
+        logger.info("Cache cleared.");
     }
 
     private String getWeatherStackUrl(String city) {
